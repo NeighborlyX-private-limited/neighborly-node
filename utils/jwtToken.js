9853a356faed159648bba2017d3926ab0a03cb3b
Parmesh Kumar <parmeshkumar414@gmail.com>
2023-12-12 16:10:18 +0530

    feat: logout added

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,7 +6,7 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRY * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: true
+   
   };
 
   res.status(statusCode).cookie("token", token, options).json({
