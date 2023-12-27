ff5407d17669173e64b59acfc44bfd36becdc997
amish <divaliciousamisha@gmail.com>
2023-12-27 23:27:41 +0530

    Added the route for adding new user in a group

--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { createGroup } = require("../controllers/groupController");
+const { createGroup, addUser } = require("../controllers/groupController");
 
 const router = express.Router();
 
 router.route("/create_group").post(createGroup);
-
+router.route("/add-user").post(addUser);
 module.exports = router;
