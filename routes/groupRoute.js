5ac9f5eadc02218f14c716f43aec778e1a787d47
Kushal Omer <kushalomer06@gmail.com>
2024-03-01 16:09:31 +0530

    API to Update Group Details

--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { isAuthenticated } = require("../middlewares/auth");
 
-const { createGroup, addUser, makeGroupPermanent, removeUser, nearestGroup, fetchLastMessages, fetchGroupDetails, nearbyUsers } = require("../controllers/groupController");
+const { createGroup, addUser, makeGroupPermanent, removeUser, nearestGroup, fetchLastMessages, fetchGroupDetails, nearbyUsers, updateGroupDetails } = require("../controllers/groupController");
 
 
 router.route("/remove-user").post(isAuthenticated, removeUser);
@@ -13,5 +13,6 @@ router.route("/nearest-group").get(isAuthenticated, nearestGroup);
 router.route("/create").post(isAuthenticated,createGroup);
 router.route("/fetch-group-messages/:groupId").get(isAuthenticated, fetchLastMessages);
 router.route("/fetch-group-details/:groupId").get(isAuthenticated, fetchGroupDetails);
+router.route("/update-group-details").put(isAuthenticated, updateGroupDetails);
 
 module.exports = router;
