5522805e3cf4e0588b3ad6f97e505bdbee7f34ea
Bharat Shukla <bharatsagii30@gmail.com>
2024-03-03 23:11:54 +0530

    changed cors origin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "http://neighborly.in.s3-website-us-east-1.amazonaws.com");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", true);
