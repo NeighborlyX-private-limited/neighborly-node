c9498a53d3da57e9014f8be749f75d914186c2a2
Aryan Mishra <codeme1223@gmail.com>
2024-03-06 22:31:27 +0530

    Slashed down the two logger files into one , also made variable name changes

--- logger/logger.js
+++ logger/logger.js
@@ -1,12 +1,29 @@
 const fs = require('fs')
 const outputLog = fs.createWriteStream('./outputLog.log');
 const errorsLog = fs.createWriteStream('./errorsLog.log');
-
-
-const consoler = new console.Console(outputLog, errorsLog);
+const fs = require('fs');
+const util = require('util');
+const fileLog = fs.createWriteStream(__dirname + '/server.log', {flags : 'w'});
+const ErrorLog = fs.createWriteStream(__dirname + '/Error.log', {flags : 'w'});
+const logOutput = process.stdout;
+console.log  = (entry) => {
+    fileLog.write(util.format(entry) + '\n');
+    logOutput.write(util.format(entry) + '\n');
+    };
+    
+    
+    console.error = (entry) => {
+    ErrorLog.write(util.format(entry) + '\n');
+    }
+    
+    
+    module.exports = {console}
+    const consoler = new console.Console(outputLog, errorsLog);
 
 
 setInterval(function () {
 consoler.log(new Date());
-consoler.error(new Error('Hey Nope'));
+consoler.error(new Error('Error'));
 }, 10000);
+
+
