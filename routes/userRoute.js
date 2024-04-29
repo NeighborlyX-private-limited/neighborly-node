d7c3ea1d4822faf96d1f9a1c72f9c94a89251aea
Kushal Omer <kushalomer06@gmail.com>
2024-04-29 20:03:48 +0530

    Change Password API implemented

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,6 +10,7 @@ const {
   updateLocation,
   fetchCities,
   fetchPreSignedURL,
+  changePassword,
   deleteUser
 } = require("../controllers/userController");
 const { isAuthenticated } = require("../middlewares/auth");
@@ -24,6 +25,7 @@ router.route("/user-info").get(isAuthenticated, userinfo);
 router.route("/get-user-groups").get(isAuthenticated, getUserGroups);
 router.route("/update-user-picture").put(isAuthenticated, updatePicture);
 router.route("/update-user-location").put(isAuthenticated, updateLocation);
+router.route("/change-password").put(isAuthenticated, changePassword);
 router.route("/delete-user").delete(isAuthenticated, deleteUser);
 router.route("/fetch-cities").get(fetchCities);
 router.route("/get-presigned-url").get(fetchPreSignedURL);
