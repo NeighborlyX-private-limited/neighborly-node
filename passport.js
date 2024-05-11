6c6d8a477ea057bac4dc08c5e06742b48edf8ce4
Kushal Omer <kushalomer06@gmail.com>
2024-05-11 19:16:57 +0530

    minor change-2

--- passport.js
+++ passport.js
@@ -9,14 +9,3 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser(function (user, done) {
     done(null, user);
 });
-
-// passport.use(new GoogleStrategy({
-//     clientID: process.env.CLIENT_ID, // Your Credentials here. 
-//     clientSecret: process.env.CLIENT_SECRET, // Your Credentials here. 
-//     callbackURL: process.env.AUTH_URL,
-//     passReqToCallback: true
-// },
-//     function (request, accessToken, refreshToken, profile, done) {
-//         return done(null, profile);
-//     }
-// ));
