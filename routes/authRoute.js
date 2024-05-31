7eade6fcd6585ae13dd1a5c59477ff5f949b40db
Aryan Mishra <codeme1223@gmail.com>
2024-05-31 15:51:30 +0530

    added logging as well as the rateLimiter

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const { isAuthenticated } = require("../middlewares/auth");
 const router = express.Router();
+const { otpLimiter } = require("../middlewares/rateLimiter");
 const passport = require("passport");
-const { 
-    loginUser,
-    registerUser,
-    logoutUser,
-    sendOTP,
-
-    googleAuth,
-    verifyOTP
-    
-
-} = require('../controllers/authController');
+const {
+  loginUser,
+  registerUser,
+  logoutUser,
+  sendOTP,
+  googleAuth,
+  verifyOTP,
+} = require("../controllers/authController");
 
 require("../middlewares/passport");
 
@@ -23,18 +21,18 @@ router.route("/login").post(loginUser);
 router.route("/register").post(registerUser);
 router.route("/logout").get(isAuthenticated, logoutUser);
 
-router.route("/send-otp").post(sendOTP);
+router.route("/send-otp").post(otpLimiter, sendOTP);
 router.route("/verify-otp").post(verifyOTP);
 
 router.route("/google/oauth").get(
-    passport.authenticate("google", {
-        successRedirect: "/user/success",
-        failureRedirect: "/user/failure",
-    })
+  passport.authenticate("google", {
+    successRedirect: "/user/success",
+    failureRedirect: "/user/failure",
+  }),
 );
 router.route("/success").get(googleAuth);
 router.route("/failure").get((req, res) => {
-    res.status(403).send("forbidden");
+  res.status(403).send("forbidden");
 });
 
 module.exports = router;
