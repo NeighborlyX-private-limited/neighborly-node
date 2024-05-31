cbd42d3270beaa51425eb66ce9ed61605b514752
Bharat Kumar Shukla <bharatsagii30@gmail.com>
2024-05-31 16:00:11 +0530

    Email verify (#74)
    * Routine dev merge to master (#69)
    
    * Neig 145 (#57)
    
    * fix: destructured user in nearbyUsers
    
    * feat: API key added
    
    * More logging done (#55)
    
    * fixed case sensitivity of email and fixed logout code (#58)
    
    * Fix change password (#60)
    
    * Change Password API implemented
    
    * Change Password API
    
    * User API to get avatar
    
    * small change in avatar-api
    
    * changed groupModel structre :: member
    
    * modifying user addition into the group
    
    * fixed a few APIs for handling user nesting in groupmodel
    
    * fix for add users API
    
    * fix in database.js and updated readme.md
    
    * updated readme.md
    
    * findMe API added (#61)
    
    * findMe API added
    
    * fixed findMe for cityCoordinates
    
    * changed variable name
    
    * Oauth2.0 with Google (#63)
    
    * Oauth2.0 with Google
    
    * fixed Oauth API
    
    * Fixed (#65)
    
    * Oauth2.0 with Google
    
    * fixed Oauth API
    
    * fixed some changes in APIs
    
    * Neig 155 (#66)
    
    * initial skeleton commit for postgres connection
    
    * fixed .env for database and pwd
    
    * Added: all Posts APIs and separated authentication APIs
    
    ---------
    
    Co-authored-by: Bharat Shukla <bharatsagii30@gmail.com>
    
    * events APIs added (#68)
    
    ---------
    
    Co-authored-by: AryanM1223 <139005437+AryanM1223@users.noreply.github.com>
    Co-authored-by: KUSHAL OMER <61742045+KUSHAL-OMER@users.noreply.github.com>
    
    * Revert "Routine dev merge to master (#69)" (#70)
    
    This reverts commit c03030d634c6959f808168e14292508ad950707f.
    
    * Dev routine merge (#71)
    
    * Neig 145 (#57)
    
    * fix: destructured user in nearbyUsers
    
    * feat: API key added
    
    * More logging done (#55)
    
    * fixed case sensitivity of email and fixed logout code (#58)
    
    * Fix change password (#60)
    
    * Change Password API implemented
    
    * Change Password API
    
    * User API to get avatar
    
    * small change in avatar-api
    
    * changed groupModel structre :: member
    
    * modifying user addition into the group
    
    * fixed a few APIs for handling user nesting in groupmodel
    
    * fix for add users API
    
    * fix in database.js and updated readme.md
    
    * updated readme.md
    
    * findMe API added (#61)
    
    * findMe API added
    
    * fixed findMe for cityCoordinates
    
    * changed variable name
    
    * Oauth2.0 with Google (#63)
    
    * Oauth2.0 with Google
    
    * fixed Oauth API
    
    * Fixed (#65)
    
    * Oauth2.0 with Google
    
    * fixed Oauth API
    
    * fixed some changes in APIs
    
    * Neig 155 (#66)
    
    * initial skeleton commit for postgres connection
    
    * fixed .env for database and pwd
    
    * Added: all Posts APIs and separated authentication APIs
    
    ---------
    
    Co-authored-by: Bharat Shukla <bharatsagii30@gmail.com>
    
    * events APIs added (#68)
    
    ---------
    
    Co-authored-by: AryanM1223 <139005437+AryanM1223@users.noreply.github.com>
    Co-authored-by: KUSHAL OMER <61742045+KUSHAL-OMER@users.noreply.github.com>
    
    * Added the otp verification for email
    
    * added logging as well as the rateLimiter
    
    * added logging as well as the rateLimiter
    
    ---------
    
    Co-authored-by: AryanM1223 <139005437+AryanM1223@users.noreply.github.com>
    Co-authored-by: KUSHAL OMER <61742045+KUSHAL-OMER@users.noreply.github.com>
    Co-authored-by: Aryan Mishra <codeme1223@gmail.com>

---
+++ utils/emailService.js
@@ -0,0 +1,43 @@
+const nodemailer = require("nodemailer");
+const otpGenerator = require("otp-generator");
+const User = require("../models/userModel");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORTE,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+const sendVerificationEmail = async (userEmail) => {
+  const otp = otpGenerator.generate(6, {
+    upperCaseAlphabets: false,
+    lowerCaseAlphabets: false,
+    specialChars: false,
+  });
+  const otpExpiry = new Date(Date.now() + 15 * 60 * 1000);
+
+  const user = await User.findOneAndUpdate(
+    { email: userEmail },
+    { otp, otpExpiry },
+    { new: true },
+  );
+
+  const mailOptions = {
+    from: `"Neighborly" <${process.env.SMTP_USER}>`,
+    to: userEmail,
+    subject: "Verify Your Email Address",
+    html: `<p>Please verify your email using the following OTP: <strong>${otp}</strong></p>`,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+module.exports = { sendVerificationEmail };
