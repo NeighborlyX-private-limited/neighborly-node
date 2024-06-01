87bc8b3cac7d0accaf1314c61cd4fcbe99e72864
Bharat Shukla <bharatsagii30@gmail.com>
2024-06-02 02:18:30 +0530

    file-upload-API-added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,12 +6,13 @@ const {
   updatePicture,
   updateLocation,
   fetchCities,
-  fetchPreSignedURL,
+  uploadFile,
   changePassword,
   deleteUser,
-  findMe
+  findMe,
 } = require("../controllers/userController");
 const { isAuthenticated } = require("../middlewares/auth");
+const { singleFileUpload } = require("../middlewares/fileUpload");
 const router = express.Router();
 
 router.route("/me").get(isAuthenticated, loggedInUser);
@@ -23,5 +24,5 @@ router.route("/change-password").put(isAuthenticated, changePassword);
 router.route("/delete-user").delete(isAuthenticated, deleteUser);
 router.route("/find-me").get(isAuthenticated, findMe);
 router.route("/fetch-cities").get(fetchCities);
-router.route("/get-presigned-url").get(fetchPreSignedURL);
+router.route("/upload-file").post(singleFileUpload, uploadFile);
 module.exports = router;
