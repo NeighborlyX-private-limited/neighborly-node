ef40df36778081864059e58b4327a58599ad69c5
Kushal Omer <kushalomer06@gmail.com>
2024-06-15 12:13:30 +0530

    added refresh token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,45 @@ const User = require("../models/userModel");
 
 
 exports.isAuthenticated = async (req, res, next) => {
-    const {token} = req.cookies;
-    if(!token)
-        return next(new ErrorHandler("You must login first", 401));
+    const { refreshToken } = req.cookies;
 
-    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+    const token = req.headers['authorization'];
 
-    req.user = await User.findById(decodedData.id);
+    const accessToken = token.substring(7);
+
+    console.log(accessToken);
     
-    next();
 
-}
+    // if(!token)
+    //     return next(new ErrorHandler("You must login first", 401));
+
+    // const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+    // req.user = await User.findById(decodedData.id);
+    
+    // next();
+    if (!accessToken && !refreshToken) {
+        return res.status(401).send('Access Denied. No token provided.');
+    }
+
+    try {
+        const decodedData = jwt.verify(accessToken, process.env.JWT_SECRET);
+        req.user = await User.findById(decodedData.id);
+        next();
+    } catch (error) {
+        if (!refreshToken) {
+            return res.status(401).send('Access Denied. No refresh token provided.');
+        }
+
+        try {
+            const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+            const user = await User.findById(decoded.id);
+            const accesstoken = user.getJWTToken(process.env.JWT_EXPIRY);
+            req.user = user;
+            req.header("Authorization", accesstoken);
+            next();
+        } catch (error) {
+            return res.status(400).send('Invalid Token.');
+        }
+    }
+};
--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,16 +1,24 @@
-const sendToken = (user, statusCode, res) => {
-  const token = user.getJWTToken();
+const User = require('../models/userModel');
+
+const sendToken = async(user, statusCode, res) => {
+  const accessToken = user.getJWTToken(process.env.JWT_EXPIRY, process.env.JWT_SECRET);
+
+  const refreshToken = user.getJWTToken(process.env.REFRESH_EXPIRY, process.env.REFRESH_SECRET);
+
+  await User.updateOne({ _id: user._id },
+    { $set: { refreshToken: refreshToken} }
+  );
 
   const options = {
     expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRY * 24 * 60 * 60 * 1000
+      Date.now() + process.env.COOKIE_EXPIRY * 30 * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
+  res.status(statusCode).cookie("refreshToken", refreshToken, options).header("Authorization", accessToken).json({
     success: true,
-    token,
+    refreshToken,
     user,
   });
   // res.setHeader("Authorization", token);
