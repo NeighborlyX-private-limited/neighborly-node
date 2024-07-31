8d6ee706c1b2f5d01161c992a082d77aef9b286f
Bharat Shukla <bharatsagii30@gmail.com>
2024-07-31 14:33:01 +0530

    fix: group API breaking

--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -24,12 +24,12 @@ router.route("/remove-user").post(isAuthenticated, removeUser);
 router.route("/make-group-permanent").put(isAuthenticated, makeGroupPermanent);
 router.route("/fetch-nearby-users").get(isAuthenticated, nearbyUsers);
 router.route("/add-user/:groupId?").post(isAuthenticated, addUser);
-router.route("/nearest-group").get(isAuthenticated, nearestGroup);
+
 router.route("/add-user").post(isAuthenticated, addUser);
 router.route("/delete-group/:groupId").delete(isAuthenticated, deleteGroup);
 router.route("/create").post(isAuthenticated, createGroup);
-router.route("/search-group").get(isAuthenticated,searchGroups);
-router.route("/report-group").post(isAuthenticated,reportGroup);
+router.route("/search-group").get(isAuthenticated, searchGroups);
+router.route("/report-group").post(isAuthenticated, reportGroup);
 router
   .route("/fetch-group-messages/:groupId")
   .get(isAuthenticated, fetchLastMessages);
