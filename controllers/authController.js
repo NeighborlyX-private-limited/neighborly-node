b7cabff108d75b683f91b1b06533f1d0fcc1469a
Bharat Kumar Shukla <bharatsagii30@gmail.com>
2024-07-31 14:27:24 +0530

    Revert "google-Authentication update (#125)" (#128)
    This reverts commit bcf9363e8f47c96be18961fb0180feb605db9a4b.

--- controllers/authController.js
+++ controllers/authController.js
@@ -194,6 +194,77 @@ exports.logoutUser = async (req, res, next) => {
   res.end();
 };
 
+exports.googleAuth = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const client = new OAuth2Client();
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      requiredAudience: process.env.CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+
+    if (!payload) {
+      errorLogger.error("Invalid token provided for Oauth");
+      res.status(400).json({
+        message: "invalid token",
+      });
+    }
+    const { email, email_verified, given_name, family_name } = payload;
+
+    if (!email_verified) {
+      errorLogger.error("Email is not verified");
+      res.status(400).json({
+        message: "Email is not verified",
+      });
+
+      let user = await User.findOne({ email: email.toLowerCase() });
+
+      if (user) {
+        if (user.isDeleted) {
+          errorLogger.error(
+            `Attempt to login with a deleted account: ${email}`
+          );
+          return res.status(401).json({
+            message: "This account has been deleted",
+          });
+        }
+
+        activityLogger.info(
+          `User ${user.username}(${user._id}) has logged in successfully via Google`
+        );
+        return sendToken(user, 200, res);
+      }
+    }
+    let username = generateUsername() + Math.floor(Math.random() * 10000);
+    while (await User.findOne({ username })) {
+      username = generateUsername() + Math.floor(Math.random() * 10000);
+    }
+    try {
+      const picture = `https://api.multiavatar.com/${username}.png?apikey=${AVATAR_KEY}`;
+      const user = await User.create({
+        username: username,
+        password: password,
+        email: email.toLowerCase(),
+        picture: picture,
+        auth_type: "email",
+      });
+      activityLogger.info("User logged in successfully");
+      sendToken(user, 200, res);
+    } catch (error) {
+      errorLogger.error("Error in Oauth:", err);
+      res.status(500).json({
+        message: err,
+      });
+    }
+  } catch (err) {
+    errorLogger.error("Error in Oauth:", err);
+    res.status(500).json({
+      message: "Error in Oauth",
+    });
+  }
+};
+
 exports.forgotPassword = async (req, res) => {
   const { email } = req.body;
   try {
