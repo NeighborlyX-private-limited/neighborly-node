883f9fcdd96414a2db90e880c006698c59c34536
Kushal Omer <kushalomer06@gmail.com>
2024-08-13 12:06:42 +0530

    search the word anywhere in wall

--- routes/wallRoute.js
+++ routes/wallRoute.js
@@ -10,10 +10,12 @@ const {
   deleteData,
   report,
   giveAward,
+  search
 } = require("../controllers/wallController");
 
 router.route("/fetch-posts").get(isAuthenticated, findPosts);
 router.route("/fetch-posts/:postId?").get(isAuthenticated, findPosts);
+router.route("/search").get(isAuthenticated, search);
 router.route("/feedback").put(isAuthenticated, feedback);
 router
   .route("/create-post")
