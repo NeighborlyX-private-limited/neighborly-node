a2a680f2325d631e7a0d97b4251b0c327444b6fc
Aryan Mishra <139005437+AryanM1223@users.noreply.github.com>
2024-08-21 16:26:19 +0530

    Made the chnages as requested by app dev and handled everything in the blast radius of changes (#135)
    * Made the chnages as requested by app dev and handled everything in the blast radius of changes
    
    * fix:isJoined and isMine working as wanted and goingEvents and myEvents API made
    
    * fix:Used ORM and changed attribute names in the model
    
    * fix:NearbyEvents
    
    * fix:avatarUrl in that one place

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -3,7 +3,7 @@ const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
-const { createEvent, getNearbyEvents, joinEvent, deleteEvent, searchEvents, updateEvent, eventDetails } = require('../controllers/eventController');
+const { createEvent, getNearbyEvents, joinEvent, deleteEvent, searchEvents, updateEvent, eventDetails, getGoingEvents, getMyEvents } = require('../controllers/eventController');
 
 
 router.route('/create-event').post(isAuthenticated, createEvent);
@@ -13,5 +13,7 @@ router.route('/event-detail/:eventId').get(isAuthenticated, eventDetails);
 router.route('/delete-event/:eventId').delete(isAuthenticated,deleteEvent);
 router.route('/search-events').get(isAuthenticated,searchEvents);
 router.route('/update-event/:eventId').put(isAuthenticated,updateEvent);
+router.route('/going-events').get(isAuthenticated,getGoingEvents);
+router.route('/my-events').get(isAuthenticated,getMyEvents);
 
 module.exports = router;
