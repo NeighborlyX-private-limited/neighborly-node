f2f0a0283478f0d85aa6241ac9daf927f5cf0631
Bharat Shukla <bharatsagii30@gmail.com>
2024-09-20 20:29:30 +0530

    feat: userFeedback added for user comments

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,6 +5,7 @@ const Comment = require("../models/CommentModel");
 const Content = require("../models/ContentModel");
 const Award = require("../models/AwardModel");
 const PollVote = require("../models/PollVoteModel");
+const CommentVote = require("../models/CommentVoteModel");
 const { Op, where } = require("sequelize");
 const { activityLogger, errorLogger } = require("../utils/logger");
 const { sequelize } = require("../config/database");
@@ -13,6 +14,7 @@ const notificationAPI = process.env.API_ENDPOINT + process.env.NOTIFICATION;
 exports.fetchCommentThread = async (req, res) => {
   try {
     const parentCommentid = req.params["id"];
+    const userId = req.user._id.toString();
 
     // Fetch comments with the given parentCommentid
     const comments = await Comment.findAll({
@@ -37,6 +39,21 @@ exports.fetchCommentThread = async (req, res) => {
       "_id picture"
     );
 
+    // Fetch user feedback (cheer/boo) for each comment
+    const commentIds = comments.map((comment) => comment.commentid);
+    const userVotes = await CommentVote.findAll({
+      where: {
+        commentid: { [Op.in]: commentIds },
+        userid: userId, // Checking votes by the current user
+      },
+    });
+
+    // Map user votes to a dictionary for easier lookup
+    const userVotesMap = userVotes.reduce((acc, vote) => {
+      acc[vote.commentid] = vote.votetype;
+      return acc;
+    }, {});
+
     // Map user details to comments
     const userMap = users.reduce((acc, user) => {
       acc[user._id] = user;
@@ -46,6 +63,9 @@ exports.fetchCommentThread = async (req, res) => {
     const formattedComments = comments.map((comment) => {
       const awards = comment.awards.map((award) => award.award_type);
       const user = userMap[comment.userid];
+
+      const userFeedback = userVotesMap[comment.commentid] || null;
+
       return {
         replyid: comment.commentid,
         text: comment.text,
@@ -56,6 +76,7 @@ exports.fetchCommentThread = async (req, res) => {
         boos: comment.boos,
         awards: awards,
         user_picture: user ? user.picture : null,
+        userFeedback: userFeedback,
       };
     });
 
@@ -74,6 +95,7 @@ exports.fetchCommentThread = async (req, res) => {
 exports.fetchComments = async (req, res) => {
   try {
     const postId = req.params["postId"];
+    const userId = req.user._id.toString();
     const page = parseInt(req.query.page) || 1;
     const pageSize = parseInt(req.query.pageSize) || 15;
 
@@ -104,7 +126,21 @@ exports.fetchComments = async (req, res) => {
     const users = await User.find({ _id: { $in: userIds } }).select(
       "username picture"
     );
-   
+
+    // Fetch user feedback (cheer/boo) for each comment
+    const commentIds = comments.rows.map((comment) => comment.commentid);
+    const userVotes = await CommentVote.findAll({
+      where: {
+        commentid: { [Op.in]: commentIds },
+        userid: userId, // Checking votes by the current user
+      },
+    });
+
+    // Map user votes to a dictionary for easier lookup
+    const userVotesMap = userVotes.reduce((acc, vote) => {
+      acc[vote.commentid] = vote.votetype;
+      return acc;
+    }, {});
 
     // Map user details to comments
     const userMap = users.reduce((acc, user) => {
@@ -115,6 +151,10 @@ exports.fetchComments = async (req, res) => {
     const formattedComments = comments.rows.map((comment) => {
       const awards = comment.awards.map((award) => award.award_type);
       const user = userMap[comment.userid];
+
+      // Check if the user has voted on the comment and get the votetype
+      const userFeedback = userVotesMap[comment.commentid] || null;
+
       return {
         commentid: comment.commentid,
         text: comment.text,
@@ -126,6 +166,7 @@ exports.fetchComments = async (req, res) => {
         award_type: awards,
         user_picture: user ? user.picture : null,
         userProfilePicture: user ? user.picture : null,
+        userFeedback: userFeedback,
       };
     });
     activityLogger.info(`Fetched comments for post ID: ${postId}`);
@@ -164,8 +205,8 @@ exports.addComment = async (req, res) => {
     });
     const content = await Content.findOne({
       where: {
-        contentid: contentid
-      }
+        contentid: contentid,
+      },
     });
     const contentUserId = content.userid;
     const contentOwner = await User.findById(contentUserId);
@@ -174,17 +215,17 @@ exports.addComment = async (req, res) => {
       method: "POST",
       body: JSON.stringify({
         token: ownerToken, //"dMVpqZWrHtPvqBHOYIsrnK:APA91bEoWJcOHLQFGyeDYYCaFqbldiLN1bwp6gE6FOUYLEySOELLevYS6_S7rvySmBLdQd7ZA6gnhaQgRPyterRwb8Vp0px8F2SsM9sl9s4Eq9hXVtPgm0wE3Vdbe8_JusSgpOKWBLin",
-        eventType: 'CommentTrigger',
+        eventType: "CommentTrigger",
         commentId: id,
         userid: contentUserId,
         title: `Comment on your Post`,
         content: `${username} has commented on your post.`,
         notificationBody: `Someone commented on your Post`,
-        notificationTitle: `Comment on your Post`
+        notificationTitle: `Comment on your Post`,
       }),
       headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
         authorization: req.headers["authorization"],
         Cookie: "refreshToken=" + req.cookies.refreshToken,
       },
