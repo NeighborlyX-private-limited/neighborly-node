cd70cd46dfed5def7ae72b5a3a87af324b246ad9
Bharat Shukla <bharatsagii30@gmail.com>
2024-11-06 13:11:52 +0530

    fix: middelware for checking user deletion

--- middlewares/userDeleted.js
+++ middlewares/userDeleted.js
@@ -1,5 +1,7 @@
+const User = require("../models/userModel");
 exports.checkActiveAccount = async (req, res, next) => {
-  const user = req.query.userId || req.user._id.toString();
+  const userid = req.query.userId || req.user._id.toString();
+  const user = await User.findById(userid);
   if (!user || user.isDeleted) {
     return res.status(403).json({ msg: "Account is inactive or deleted" });
   }
