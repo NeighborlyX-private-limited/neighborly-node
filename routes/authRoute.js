5399b88d89c92100c9f182d90225bfee207d3097
Bharat Shukla <bharatsagii30@gmail.com>
2024-11-28 19:20:43 +0530

    fix: otp limiter at correct place

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -18,14 +18,14 @@ router.route("/login").post(loginUser);
 router.route("/register").post(registerUser);
 router.route("/logout").get(isAuthenticated, logoutUser);
 
-router.route("/send-otp").post(otpLimiter, sendOTP);
+router.route("/send-otp").post(sendOTP);
 
 router.route("/verify-otp").post(verifyOTP);
 router.route("/forgot-password").post(forgotPassword);
 
 router.route("/google/login").post(googleAuth);
 
-router.route("/send-phone-otp").post(sendPhoneOTP);
+router.route("/send-phone-otp").post(otpLimiter, sendPhoneOTP);
 router.route("/verify-phone-otp").post(verifyPhoneOTP);
 
 module.exports = router;
